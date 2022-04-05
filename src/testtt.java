f18356f3192865beb421a6d330881e828faba701
Andrei-Brailescu <101254172+Andrei-Brailescu@users.noreply.github.com>
2022-04-05 23:40:21 +0300

    Am testat functiile de desenare

---
+++ src/testtt.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class testtt {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+		
+		BufferedImage img = ImageIO.read(new File("poza.jpeg"));
+		Graphics2D g2d = img.createGraphics();
+		g2d.setColor(Color.RED);
+		g2d.drawRect(200, 200, 300, 300);
+		g2d.dispose();
+	
+		    File outputfile = new File("saved.jpeg");
+		    ImageIO.write(img, "jpeg", outputfile);
+		  
+	
+
+	}
+
+}
