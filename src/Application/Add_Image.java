3f679f26bb2a052c1e1f2645bbc3be39b2e38dfc
Stoleriu Stefan <stefan.stoleriu@student.tuiasi.ro>
2022-04-10 00:47:28 +0300

    Am reorganizat clasele

---
+++ src/Application/Add_Image.java
@@ -0,0 +1,51 @@
+package Application;
+
+import static marvin.MarvinPluginCollection.scale;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import marvin.gui.MarvinImagePanel;
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+
+public class Add_Image {
+	
+	private MarvinImagePanel imagePanel; 
+	private MarvinImage	image;  
+	private MarvinImage backupImage;
+	private JButton Add_Image;
+	private JFrame frame;
+	
+	public Add_Image(JFrame frame, JButton Add_Image) {
+		this.frame = frame;
+		this.Add_Image = Add_Image;
+	}
+	
+	public void Load() {
+		Add_Image.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				int response = fileChooser.showOpenDialog(null);
+				
+				if (response == JFileChooser.APPROVE_OPTION) {
+					File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+					System.out.println(file.toString());
+					imagePanel = new MarvinImagePanel();
+					imagePanel.setBounds(10, 10, 800, 543);
+					frame.getContentPane().add(imagePanel);
+					image = MarvinImageIO.loadImage(file.toString());
+			        backupImage = image.clone();
+			        scale(backupImage, image, 800, 543);
+			        imagePanel.setImage(image); 
+				}
+			}
+		});
+	}
+
+}
---
+++ src/Application/GUI.java
@@ -0,0 +1,48 @@
+package Application;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GUI {
+	
+	private JFrame frame;
+	private JButton Add_Image;
+	private JButton Select_Object;
+	private JButton Export;
+	
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+		Add_Image imagine = new Add_Image(frame, Add_Image);
+		imagine.Load();
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1050, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("DataSet");
+		frame.getContentPane().setLayout(null);
+		
+		Add_Image = new JButton("Add Image");
+		Add_Image.setBounds(839, 152, 187, 41);
+		frame.getContentPane().add(Add_Image);
+		
+		Select_Object = new JButton("Select Object");
+		Select_Object.setBounds(839, 220, 187, 41);
+		frame.getContentPane().add(Select_Object);
+		
+		Export = new JButton("Export");
+		Export.setBounds(839, 288, 187, 41);
+		frame.getContentPane().add(Export);
+	}
+
+		public JFrame getFrame() {
+			return frame;
+		}
+}
