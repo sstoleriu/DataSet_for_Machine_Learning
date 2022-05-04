b2cc45475ba734a6a99d6858f1048f24e8985273
GabrielLeonte00 <gabriel16.leonte@gmail.com>
2022-05-04 19:15:10 +0300

    Working program

--- src/Application/classes/Export.java
+++ src/Application/classes/Export.java
@@ -3,14 +3,12 @@ package Application.classes;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Vector;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-
-import com.sun.javafx.geom.Rectangle;
 
 import Application.interfaces.Obiect;
 import static marvin.MarvinPluginCollection.*;
@@ -24,7 +22,7 @@ public class Export{
 	private Vector<Obiect> listOfObjects;
 	
 	public Export(JFrame frame,JButton Export, Add_Image Add_image, Vector<Obiect> listOfObjects){
-		this.Add_image = Add_image;
+		Application.classes.Export.Add_image = Add_image;
 		this.Export=Export;
 		this.listOfObjects = listOfObjects;
 	}
@@ -44,18 +42,15 @@ public class Export{
 				fileChooser2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				int response = fileChooser2.showOpenDialog(null);
 				File file2 = fileChooser2.getSelectedFile();
-				VctCrop vct1=new VctCrop(listOfObjects);
-			 	Vector<String> allvct=vct1.getvct();
-			 	VctFreq vct2=new VctFreq(allvct);
-			 	String namevct[]=vct2.getName_VctFreq();
-			 	Integer contorvct[]=vct2.getContor_VctFreq();
-			 	Integer aux[]=contorvct;
-			 	int conti;
-			 	/*
-			 	 * test <---
-			 	 */
-				
-				/*if (response == JFileChooser.APPROVE_OPTION) {	 	
+
+				if (response == JFileChooser.APPROVE_OPTION) {
+					VctCrop vct1=new VctCrop(listOfObjects);
+				 	Vector<String> allvct=vct1.getvct();
+				 	VctFreq vct2=new VctFreq(allvct);
+				 	String namevct[]=vct2.getName_VctFreq();
+				 	Integer contorvct[]=vct2.getContor_VctFreq();
+				 	Integer aux[]=Arrays.copyOf(contorvct, contorvct.length);
+				 	int conti;
 					 	for(int i=0;i<allvct.size();i++){
 					 		for(int j=0;j<namevct.length;j++){
 					 			if(allvct.get(i).equals(namevct[j])){
@@ -63,39 +58,10 @@ public class Export{
 					 				crop(imageIn,imageOut,rect.x,rect.y,rect.width,rect.height);
 					 				conti=contorvct[j]-aux[j];
 					 				aux[j]=aux[j]-1;
-					 				MarvinImageIO.saveImage(imageOut, file2.getAbsolutePath()+'/'+allvct.get(i)+conti+".jpg");
-					 				//crop(imageIn,imageOut,vect_poz_x[i][0],vect_poz_y[i][0],vect_width[i][0],vect_height[i][0];
-						  			//MarvinImageIO.saveImage(imageOut, file.getAbsolutePath()+'/'+ta[i]+".jpg");
-						  			//contorvct[j]--;
+					 				MarvinImageIO.saveImage(imageOut, file2.getAbsolutePath()+'/'+allvct.get(i)+"("+conti+").jpg");
 					 			}
 					 		}
-					 	}*/
-					 	
-					/*
-						 //y[i]-vector cu numarul de taguri ale tagurilor respective
-						 //ta[i]-vector cu denumirile tagurilor
-						 //numarul de pe pozitia 0 a vectorului y corespunde cu numele de pe pozitia 0 a vectorului ta
-						 //arr_poz_x matrice cu pozitia x ale spatiilor selectate depinzand pe linie de numele tagului, iar coloane numarul de taguri
-						 //arr_poz_y matrice cu pozitia y ale spatiilor selectate depinzand pe linie de numele tagului, iar coloane numarul de taguri
-						 //arr_width matrice cu latimea spatiilor selectate depinzand pe linie de numele tagului, iar coloane numarul de taguri
-						 //arr_height matrice cu inaltimea spatiilor selectate depinzand pe linie de numele tagului, iar coloane numarul de taguri
-						  for(int i=0;i<nr_total_de_taguri;i++)
-						  	int aux=y[i];
-						  	if(aux!=0){
-						  		crop(imageIn,imageOut,vect_poz_x[i][0],vect_poz_y[i][0],vect_width[i][0],vect_height[i][0];
-						  		MarvinImageIO.saveImage(imageOut, file.getAbsolutePath()+'/'+ta[i]+".jpg");
-        						aux--;
-						  	}
-						  	while(aux!=0){
-						  		crop(imageIn,imageOut,arr_poz_x[i][y[i]-aux],arr_poz_y[i][y[i]-aux],arr_width[i][y[i]-aux],arr_height[i][y[i]-aux];
-						  		MarvinImageIO.saveImage(imageOut, file.getAbsolutePath()+'/'+ta[i]+'('+(y[i]-aux)+')'+".jpg");
-        						aux--;
-						  	}
-						 */
-					
-						/*int x=60,y=32,width=182,height=62;
-						crop(imageIn, imageOut, x, y, width, height);
-						MarvinImageIO.saveImage(imageOut, file2.getAbsolutePath()+"/x.jpg");*/
+					 	}
 				}
 			}
 		});
