b876c7e86efd35d934d3f1199e28eea3b05606cc
GabrielLeonte00 <gabriel16.leonte@gmail.com>
2022-05-19 19:46:08 +0300

    Modified helpr

--- src/Application/classes/Helper.java
+++ src/Application/classes/Helper.java
@@ -61,7 +61,6 @@ public class Helper {
 	                frame.setLocation(X,Y);
 	        }
 	    });
-		//"font-family:'Comic Sans MS'
 		JEditorPane textHelp = new JEditorPane();
 		textHelp.setBounds(10, 15, 364, 68);
 		textHelp.setEditable(false);
@@ -69,21 +68,19 @@ public class Helper {
 		textHelp.setContentType("text/html");
 		textHelp.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 		textHelp.setText("<center><span style=\"font-family:'Comic Sans MS'; color:white;\" >Welcome to the helper of DataSet!<br>"
-				+ "Press the buttons to get info about them<br>"
+				+ "Press the buttons to get info about them from DataSet's brother, DataHelp<br>"
 				+ "The following buttons are for the main menu</span>");
 		frame.getContentPane().add(textHelp);
 		
-		
 		btnAddImage = new JButton("Insert image");
-		btnAddImage.setFocusPainted(false);
+		btnAddImage.setFocusable(false);
 		btnAddImage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will be used for inserting the image that you want to use\r\n"
-						+ "It is needed to enable the button [Refresh], the button [Export] and the tag list\r\n"
-						+ "Inserting something other than images will display the following warning\r\n"
-						+ "Warning: Please insert an image","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
-				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				JOptionPane.showMessageDialog(mesaj,"Buhuhu, with this button you will\r\n"
+						+ "Insert an image in the program.\r\n"
+						+ "It will also enable the other functions\r\n","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE,new FileManager().getIcon("DataHelp.png"));
+				mesaj.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 		});
 		btnAddImage.setBounds(30, 93, 143, 56);
@@ -91,14 +88,12 @@ public class Helper {
 		frame.getContentPane().add(btnAddImage);
 		
 		btnTagMenu = new JButton("Tag menu");
-		btnTagMenu.setFocusPainted(false);
+		btnTagMenu.setFocusable(false);
 		btnTagMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will open the tag menu\r\n"
-						+ "The menu has a list in which the curent existing tags are deposited\r\n"
-						+ "Selecting a tag from the list will display the tag's color \r\n"
-						+ "The color will be shown in a rectangle situated at the left bottom of the menu","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhu, this button will open the tag menu\r\n"
+						+ "In this menu you can add/edit/delete tags\r\n","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -111,10 +106,8 @@ public class Helper {
 		btnRefresh.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button is disabled until an image is added\r\n"
-						+ "You can do that by pressing the button [Insert image]\r\n"
-						+ "After inserting an image the button will be able to refresh the image\r\n"
-						+ "You can use the button to remove all the tags and create others","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhu, this button will clean the image\r\n"
+						+ "So that you can draw other rectangles","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -123,17 +116,14 @@ public class Helper {
 		frame.getContentPane().add(btnRefresh);
 		
 		btnExport = new JButton("Export");
-		btnExport.setFocusPainted(false);
+		btnExport.setFocusable(false);
 		btnExport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button is disabled until an image is added\r\n"
-						+ "You can do that by pressing the button [Insert image]\r\n"
-						+ "After being pressed the button will export the drawn rectangles in a selected folder\r\n"
-						+ "Not drawing any rectangle will display the following warning\r\n"
-						+ "Warning: Please, select objects to export\r\n"
-						+ "Successfully exporting the images will display the following message\r\n"
-						+ "Message: Export done!","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"With this button you will export\r\n"
+						+ "All drawn rectangles on the image\r\n"
+						+ "Simply choose a folder, buhuhu\r\n"
+						+ "And we will save them there","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -150,17 +140,13 @@ public class Helper {
 		frame.getContentPane().add(textHelp2);
 		
 		btnNewTag = new JButton("New");
-		btnNewTag.setFocusPainted(false);
+		btnNewTag.setFocusable(false);
 		btnNewTag.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will permit you to add another tag\r\n"
-						+ "You'll have to insert the name that you want to use and choose a color\r\n"
-						+ "The tag will get the preselected color in case of not choosing one\r\n"
-						+ "You cannot add a tag with the same name neither with an empty name\r\n"
-						+ "Otherwise the following warnings will be dysplayed accordingly\r\n"
-						+ "Warning 1: This name is already used by another tag\r\n"
-						+ "Warning 2: This name is invalid","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"This button creates a new tag\r\n"
+						+ "Buhuhu, but keep this in mind\r\n"
+						+ "No null name nor an existing one\r\n","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -169,17 +155,12 @@ public class Helper {
 		frame.getContentPane().add(btnNewTag);
 		
 		btnEditName = new JButton("Name");
-		btnEditName.setFocusPainted(false);
+		btnEditName.setFocusable(false);
 		btnEditName.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will permit you to rename a chosen tag from the list\r\n"
-						+ "Not selecting the tag before pressing the button will display the following warning\r\n"
-						+ "Warning: Please select the tag you want to rename\r\n"
-						+ "You cannot rename a tag with a name already taken or an empty name\r\n"
-						+ "Otherwise the following warnings will be dysplayed accordingly\r\n"
-						+ "Warning 1: This name is already used by another tag\r\n"
-						+ "Warning 2: This name is invalid","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhuhu, with this button you change\r\n"
+						+ "The names of your existing tags","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -188,13 +169,12 @@ public class Helper {
 		frame.getContentPane().add(btnEditName);
 		
 		btnEditColor = new JButton("Color");
-		btnEditColor.setFocusPainted(false);
+		btnEditColor.setFocusable(false);
 		btnEditColor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will permit you to change the color of a chosen tag from the list\r\n"
-						+ "Not selecting the tag before pressing the button will display the following warning\r\n"
-						+ "Warning: Please select the tag you want to change its color","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhuhu, with this button you change\r\n"
+						+ "The colors of your existing tags","Info bufufu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -203,13 +183,13 @@ public class Helper {
 		frame.getContentPane().add(btnEditColor);
 		
 		btnDeleteTag = new JButton("Delete");
-		btnDeleteTag.setFocusPainted(false);
+		btnDeleteTag.setFocusable(false);
 		btnDeleteTag.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"This button will permit you to delete a chosen tag from the list\r\n"
-						+ "Not selecting the tag before pressing the button will display the following warning\r\n"
-						+ "Warning: Please select the tag you want to delete","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhu, unwanted tag in your list?\r\n"
+						+ "Fear not, my user! This button!\r\n"
+						+ "Will delete it for you, buhuhu!","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -226,15 +206,13 @@ public class Helper {
 		frame.getContentPane().add(textHelp3);
 		
 		btnDrawing = new JButton("How to draw");
-		btnDrawing.setFocusPainted(false);
+		btnDrawing.setFocusable(false);
 		btnDrawing.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"In order to draw you first need to insert a picture\r\n"
-						+ "	Drawing an object will use the list situated in the right bottom of the main menu\r\n"
-						+ "	Choose a tag and then draw on the image by clicking on a place on the image\r\n"
-						+ "	The second click will create a rectangle between the two points drawing it\r\n"
-						+ "	The rectangle will be drawn using the color of the chosen tag","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhuhu, first you will need to add an image\r\n"
+						+ "Then draw an rectangle with two clicks\r\n"
+						+ "Surround the wanted object with your rectangle","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -243,13 +221,12 @@ public class Helper {
 		frame.getContentPane().add(btnDrawing);
 		
 		btnDelTag = new JButton("Deleting");
-		btnDelTag.setFocusPainted(false);
+		btnDelTag.setFocusable(false);
 		btnDelTag.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"The tag list should have at least one item in it\r\n"
-						+ "	Trying to remove it will display the following warning\r\n"
-						+ "	Warning: The tag list should have at least one item in it. Do not remove the last tag\r\n","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhu, this is a little info\r\n"
+						+ "You cannot delete your last tag","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -258,14 +235,13 @@ public class Helper {
 		frame.getContentPane().add(btnDelTag);
 		
 		btnExporting = new JButton("Exporting");
-		btnExporting.setFocusPainted(false);
+		btnExporting.setFocusable(false);
 		btnExporting.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj," The export function will save in a folder the drawn rectangles of the inserted image\r\n"
-						+ "	They will be saved as .jpg with the names \"Tag name (number)\"\r\n"
-						+ "	The program will save the images over the existing ones\r\n"
-						+ "	Therefore we sugest to save in different folders for each image\r\n","Info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Buhu, your wanted cropped images?\r\n"
+						+ "Properly saved in your chosen folder\r\n"
+						+ "With correct names and numbers ","Info buhuhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -274,14 +250,12 @@ public class Helper {
 		frame.getContentPane().add(btnExporting);
 		
 		btnWarning = new JButton("Warning");
-		btnWarning.setFocusPainted(false);
+		btnWarning.setFocusable(false);
 		btnWarning.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"Notify: Do not attempt to modify the resources from outside the app\r\n"
-						+ "	Otherwise the program will not work as intended and fatal errors will occur\r\n"
-						+ "	The two files containing the names and the colors should always have the same number of lines\r\n"
-						+ "	Please do not modify the files in order for the program to function properly\r\n","Warning info",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+				JOptionPane.showMessageDialog(mesaj,"Please, do not attempt this\r\n"
+						+ "Trying to modify our files\r\n","Warning info buhu!",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("DataHelp.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
@@ -291,7 +265,6 @@ public class Helper {
 		//////////////
 		help.addActionListener(new ActionListener() {
 			
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(OK) {
 					frame.setVisible(false);
@@ -307,23 +280,21 @@ public class Helper {
 		
 		btnCredits.addActionListener(new ActionListener() {
 			
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFrame mesaj = new JFrame();
-				JOptionPane.showMessageDialog(mesaj,"The program was made within the project from the second semester\r\n"
-						+ "In the third year of university of the following students registered\r\n"
-						+ "In Faculty of Automatic Control and Computer Engineering from Iasi:\r\n"
+				JOptionPane.showMessageDialog(mesaj,"Students from AC Iasi\r\n"
 						+ "-Stoleriu Stefan-\r\n"
 						+ "-Ignat Mihaela-Loredana-\r\n"
 						+ "-Brailescu Andrei-\r\n"
 						+ "-Leonte Gabriel-\r\n"
-						+ "Added icons created by Icongeek26 from flaticon.com\r\n"
-						+ "Added icons created by Freepik from flaticon.com\r\n"
-						+ "Mascot created by MrHamster from flaticon.com\r\n","Credits",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
+						+ "Icons by Icongeek26 on flaticon.com\r\n"
+						+ "Icons by Freepik on flaticon.com\r\n"
+						+ "Mascots by MrHamster on flaticon.com\r\n","Credits",JOptionPane.INFORMATION_MESSAGE, new FileManager().getIcon("info.png"));
 				mesaj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
 	}
+	
 	public void changeHelpIcons(int cont) {
 		btnAddImage.setIcon(new FileManager().getIcon(cont+"insert.png"));
 		btnTagMenu.setIcon(new FileManager().getIcon(cont+"tagmenu.png"));
